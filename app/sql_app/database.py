0f2e7b51ee942415ac7da80ec81cf72a9583ad74
emir <emir.afwan@gmail.com>
2021-06-04 18:53:33 +0700

    config to connect to cloud sql

--- app/sql_app/database.py
+++ app/sql_app/database.py
@@ -2,10 +2,34 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
  
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://test:test123@127.0.0.1/testapi"
+
+db_user = os.environ["DB_USER"]
+db_pass = os.environ["DB_PASS"]
+db_name = os.environ["DB_NAME"]
+db_socket_dir = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
+cloud_sql_connection_name = os.environ["CLOUD_SQL_CONNECTION_NAME"]
+
+engine = sqlalchemy.create_engine(
+    # Equivalent URL:
+    # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
+    sqlalchemy.engine.url.URL.create(
+        drivername="mysql+pymysql",
+        username=db_user,  # e.g. "my-database-user"
+        password=db_pass,  # e.g. "my-database-password"
+        database=db_name,  # e.g. "my-database-name"
+        query={
+            "unix_socket": "{}/{}".format(
+                db_socket_dir,  # e.g. "/cloudsql"
+                cloud_sql_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
+        }
+    ),
+    **db_config
+)
+
+"""SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://admin:password@/data_col127.0.0.1/testapi"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL)
+    SQLALCHEMY_DATABASE_URL)"""
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
